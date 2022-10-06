717f3afce3da9aab50a6004745a02176db462f40
mao <1296193245@qq.com>
2022-10-06 12:56:59 +0800

    android_TableLayout

--- app/src/main/java/mao/android_tablelayout/MainActivity.java
+++ app/src/main/java/mao/android_tablelayout/MainActivity.java
@@ -40,5 +40,13 @@ public class MainActivity extends AppCompatActivity
                 startActivity(new Intent(MainActivity.this, MainActivity4.class));
             }
         });
+        findViewById(R.id.Button4).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(MainActivity.this, MainActivity5.class));
+            }
+        });
     }
 }
