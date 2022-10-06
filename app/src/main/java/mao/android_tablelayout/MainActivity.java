545621fbc688b5ebad69415a37d957617e2e3747
mao <1296193245@qq.com>
2022-10-06 12:29:28 +0800

    android_TableLayout

--- app/src/main/java/mao/android_tablelayout/MainActivity.java
+++ app/src/main/java/mao/android_tablelayout/MainActivity.java
@@ -2,7 +2,10 @@ package mao.android_tablelayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -12,5 +15,14 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.Button1).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(MainActivity.this, MainActivity2.class));
+            }
+        });
     }
 }
