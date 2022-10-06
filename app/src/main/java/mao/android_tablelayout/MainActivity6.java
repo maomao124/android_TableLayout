3eb1689fd29400c2f1de32ac0a1a5c500f55fa49
mao <1296193245@qq.com>
2022-10-06 14:57:07 +0800

    android_TableLayout

--- app/src/main/java/mao/android_tablelayout/MainActivity6.java
+++ app/src/main/java/mao/android_tablelayout/MainActivity6.java
@@ -1,11 +1,15 @@
 package mao.android_tablelayout;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -39,7 +43,7 @@ public class MainActivity6 extends AppCompatActivity
 
         TableLayout tableLayout = findViewById(R.id.TableLayout);
 
-        for (int i = 0; i < 100; i++)
+        for (int i = 0; i < 500; i++)
         {
             int id = 100000 + i + 1;
             String name = UUID.randomUUID().toString().substring(0, 6);
@@ -53,8 +57,50 @@ public class MainActivity6 extends AppCompatActivity
             textView_name.setText(name);
             TextView textView_sex = view.findViewById(R.id.sex);
             textView_sex.setText(sex);
+            if (sex.equals("男"))
+            {
+                textView_sex.setTextColor(Color.rgb(130, 80, 255));
+            }
             TextView textView_age = view.findViewById(R.id.age);
             textView_age.setText(String.valueOf(age));
+
+            TableRow tableRow = view.findViewById(R.id.TableRow);
+            tableRow.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    new AlertDialog.Builder(MainActivity6.this)
+                            .setTitle("更新id为" + id + "的信息")
+                            .setMessage("\n\n\n\n\n\n\n\n\n\n")
+                            .create()
+                            .show();
+                }
+            });
+
+            tableRow.setOnLongClickListener(new View.OnLongClickListener()
+            {
+                @Override
+                public boolean onLongClick(View v)
+                {
+                    new AlertDialog.Builder(MainActivity6.this)
+                            .setTitle("删除提示")
+                            .setMessage("是否删除id为" + id + "的信息？")
+                            .setPositiveButton("确定删除", new DialogInterface.OnClickListener()
+                            {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which)
+                                {
+                                    tableLayout.removeView(view);
+                                }
+                            })
+                            .setNeutralButton("取消", null)
+                            .create()
+                            .show();
+                    return true;
+                }
+            });
+
             tableLayout.addView(view);
         }
     }
